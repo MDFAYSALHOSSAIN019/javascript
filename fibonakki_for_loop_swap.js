ec65501c8626c9ce5f71ac550dd0d7b54da83425
MD FAYSAL HOSSAIN <mdfaysalhossain019@gmail.com>
2023-05-22 13:03:37 +0600

    add for_swep_if_switch

---
+++ fibonakki_for_loop_swap.js
@@ -0,0 +1,48 @@
+// 0 1 1 2 3 5 8 13 21 34.......fibonakki count
+
+
+// let num1=0;
+// let num2=1;
+// let sum=0;
+
+// console.log(num1+ "\n" +num2)
+
+// for(let i=3; i<=10; i++){
+// sum=num1+num2;
+// console.log(sum);
+// num1=num2;
+// num2=sum;
+// }
+
+// let n1=0;
+// let n2=1;
+// let sum=(n1+n2);
+// console.log(n1+ "\n" +n2);
+
+// for(let i=3; i<=10; i++){
+// sum=n1+n2;
+// console.log(sum);
+// n1=n2;
+// n2=sum;
+
+
+// }
+
+let number1=0;
+let number2=1;
+let sum=number1+number2;
+
+console.log(number1+ "\n" +number2);
+
+for(let i=3; i<=20; i++ ){
+   sum=number1+number2;
+   console.log(sum);
+   number1=number2;
+   number2=sum;
+
+}
+
+
+
+
+
---
+++ if_else.js
@@ -0,0 +1,48 @@
+
+let userName="html";
+let password="hi13";
+
+if(userName=="html"){
+    // console.log("well-come "+ password);
+ if(password=="hi123"){
+    console.log("well-come " + userName);
+}
+else{
+    console.log("password is incorrect");
+}
+}
+
+else{
+    console.log("user not founded");
+
+}
+
+
+// result sheet---------------
+
+// let i=75;
+// if(i<=32){
+// console.log("F");
+// }
+//  else if(i<=39){
+//     console.log("D");
+//  }
+//  else if(i<=49){
+//     console.log("C");
+//  }
+//  else if(i<=59){
+//     console.log("B");
+//  }
+//  else if(i<=69){
+//     console.log("A-");
+//  }
+//  else if(i<=79){
+//     console.log("A");
+//  }
+//  else if(i<=100){
+//     console.log("A+");
+//  }
+
+// else{
+//     // console.log("F");
+// }
