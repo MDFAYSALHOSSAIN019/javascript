99856b56d5ea4f2e89a16698cbc7627021382f94
MD FAYSAL HOSSAIN <mdfaysalhossain019@gmail.com>
2023-05-27 13:30:34 +0600

    add

---
+++ node text/Test-if_istatment.js
@@ -0,0 +1,135 @@
+// let x=5;
+// let y=7;
+
+// if(x==y){
+// console.log("true");
+// }
+// else{
+// console.log("false");
+// }
+
+// let hi=256851488554885;
+
+// if(hi%7==4){
+//     console.log("you are right")
+// }
+
+// else{
+//     console.log("you are wrong")
+// }
+
+
+
+// else if ar uses
+
+// let weather=22;
+// if(weather>=28){
+// console.log("abnormal");
+// }
+// else if(weather>=19){
+//     console.log("normal");
+
+// }
+// else if(weather>=0){
+//     console.log("cool");
+
+// }
+// else{
+//     console.log("invalid weather");
+// }
+
+// count odd number
+
+
+
+
+
+// for(let my=1; my<=100; my++){
+
+// if(my%2==1){
+//     console.log(my);
+// }
+
+// else{
+    
+// }
+
+// }
+
+// for(let my=1; my<=100; my++){
+
+//     if(my%2==0){
+       
+//     }
+    
+//     else{
+//         console.log(my);
+//     }
+    
+//     }
+
+//     for(let my=1; my<=100; my++){
+
+//         if(my%2!=0){
+//             console.log(my);
+//         }
+        
+//         else{
+            
+//         }
+        
+//         }
+//   jai number 3 ,5 dara divide hoi sei number gula-------------------------------------
+// for(hello=1; hello<=50; hello++){
+//     if(hello%3==0){
+//         console.log(hello)
+//     }
+//     else if(hello%5==0){
+
+//         console.log(hello);
+//     }
+//     else{
+
+//     }
+// }
+
+// && , || , ! and, or, not logical oparator uses---------------------------------------
+     
+// for(hello=1; hello<=50; hello++){
+//     if(hello%3==0 || hello%5==0){
+//         console.log(hello);
+//     }
+//     else{
+
+//     }
+// }
+
+// for(hello=1; hello<=50; hello++){
+//     if(hello%3==0 && hello%5==0){
+//         console.log(hello);
+//     }
+//     else{
+
+//     }
+// }
+
+// for(hello=1; hello<=50; hello++){
+//     if(hello%3==0 ! hello%5==0){
+//         console.log(hello);
+//     }
+//     else{
+
+//     }
+// }
+// ! factorial ar man ber korar method--------------------------------------------------
+// let input=4;
+// let result=1;
+
+// for(hi=input; hi>0; hi--){
+
+// if(result=result*=hi){
+
+// }
+
+// }
+// console.log(result);
