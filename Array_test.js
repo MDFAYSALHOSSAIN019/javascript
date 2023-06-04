bc9b65321494955661791e4f621a06cad6747ac4
MD FAYSAL HOSSAIN <mdfaysalhossain019@gmail.com>
2023-06-04 13:11:54 +0600

    add

--- Array_test.js
+++ Array_test.js
@@ -1,12 +1,30 @@
-let names=[5];
+// let names=[5];
 
-names[0]="faysal";
-names[1]="hossain";
-names[2]="how";
-names[3]="are";
-names[4]="you";
+// names[0]="faysal";
+// names[1]="hossain";
+// names[2]="how";
+// names[3]="are";
+// names[4]="you";
 
-console.log(names);
+// console.log(names);
+
+let myarry=["hi","how","are","you"];
+let myage=[20,50,30,50];
+
+for(let i=0; i<myarry.length; i++){
+    document.write(myarry+"br");
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ sereal number.js
@@ -0,0 +1,8 @@
+
+for(let i=1; i<=50; i++){
+    if(i==7 || i==15 || i==26 || i==35){
+        continue;
+        
+    }
+    document.write("<p>"+i+"<p>")
+}
